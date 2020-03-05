bdc6df58d0b51c532b8ba4f318adbb8eab924d70
Logan Jaeger <loganjaeger@berkeley.edu>
2020-03-04 18:40:23 -0800

    d

--- NST.py
+++ NST.py
@@ -59,7 +59,7 @@ def make_discriminator():
 def make_generator(latent_dim = 100):
 	model = Sequential()
 	model.add(Dense(30 * 25 * 25, activation = "relu", input_shape = (latent_dim,)))
-	model.add(Reshape((30, 25, 25)))
+	model.add(Reshape((25, 25, 30)))
 	model.add(UpSampling2D(size = (3, 3)))
 	model.add(Conv2D(8*conv_scale, kernel_size = kernel_size, padding = "same"))
 	model.add(BatchNormalization(momentum = .95))
